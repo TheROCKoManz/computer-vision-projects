1a69065cb22c7677a222c2ed28d15163527da5c6
THEROCKOMANZ\Manz <manasji.haldar@hyperspace-consulting.com>
2023-07-08 18:34:27 +0530

    Changes in Facial_Recognition/PredictionPipeline.py function

--- Facial_Recognition/PredictionPipeline.py
+++ Facial_Recognition/PredictionPipeline.py
@@ -4,6 +4,6 @@ import sys
 
 if __name__ == '__main__':
     img = sys.argv[1]
-    modelfile = sys.argv[2]
+    modelfile = 'Data/Trained_Model_Garden/'+sys.argv[2]
     img = image.load_img(img, target_size=(256, 256, 3))
     predict(image,modelfile)
