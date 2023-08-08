3cee71f8430fef27dd1b3a5b43e36768a3096db4
Manasij <manasij@hyperspace-consulting.com>
2023-08-09 00:57:19 +0530

    Working on WebUI

--- Facial_Recognition/Database_Connect/Load_Users.py
+++ Facial_Recognition/Database_Connect/Load_Users.py
@@ -18,17 +18,19 @@ def insert_user_info(user_info):
     try:
 
         insert_query = """
-            # INSERT INTO UserInfo (FirstName, LastName, Age, Ethnicity, Gender)
-            # VALUES (%s, %s, %s, %s, %s, %s)
-            # """
-        cursor.execute(insert_query, (user_info['first_name'], user_info['last_name'], user_info['age'], user_info['ethnicity'], user_info['gender']))
+                    INSERT INTO Users (FirstName, LastName, Age, Ethnicity, Gender)
+                    VALUES (%s, %s, %s, %s, %s)
+                """
+        data = (user_info['FirstName'], user_info['LastName'], int(user_info['Age']), user_info['Ethnicity'], user_info['Gender'])
+
+        cursor.execute(insert_query, data)
         connection.commit()
         print(f'''...User Recorded...
-        FirstName: {user_info["first_name"]}, 
-        LastName: {user_info['last_name']}, 
-        Age: {user_info['age']}, 
-        Race: {user_info['ethnicity']}, 
-        Gender: {user_info['gender']})''')
+        FirstName: {user_info["FirstName"]}, 
+        LastName: {user_info['LastName']}, 
+        Age: {user_info['Age']}, 
+        Race: {user_info['Ethnicity']}, 
+        Gender: {user_info['Gender']})''')
 
     except mysql.connector.Error as err:
         print(f"Error: {err}")
@@ -39,43 +41,43 @@ def insert_user_info(user_info):
 
 
 
--------------------------------------------------------------------------------------------------------------
-
-def load_user_data_toSQL(XL_file):
-    table = pd.DataFrame(pd.read_excel(XL_file))
-    table = table.where(pd.notna(table), None)
-    connection = DB_Connect()
-    cursor =connection.cursor()
-    try:
-        truncate_query = f"TRUNCATE TABLE Users"
-        cursor.execute(truncate_query)
-
-        # Reset the auto-increment value to 1
-        # reset_query = f"ALTER TABLE UserDetails AUTO_INCREMENT = 00001"
-        # cursor.execute(reset_query)
-
-        insert_query = """
-            INSERT INTO Users (UserID, FirstName, LastName, Age, Ethnicity, Gender)
-            VALUES (%s, %s, %s, %s, %s, %s)
-        """
-        for index, row in table.iterrows():
-
-            UserID = 'User_'+"{:03d}".format(index+1)
-            data = (UserID, row['FirstName'], row['LastName'], int(row['Age']), row['Ethnicity'], row['Gender'])
-            print(data)
-            cursor.execute(insert_query,data)
-        connection.commit()
-        print('\nData Loaded!')
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-
-    cursor.close()
-
-    DB_Disconnect(connection)
-
-
-
-load_user_data_toSQL(r'M:\Private\Softwares\Python Envs\HyperSpace\ComputerVision\Data\Data_Load_to_sqlDB\UserDetails.xlsx')
+#-------------------------------------------------------------------------------------------------------------
+#
+# def load_user_data_toSQL(XL_file):
+#     table = pd.DataFrame(pd.read_excel(XL_file))
+#     table = table.where(pd.notna(table), None)
+#     connection = DB_Connect()
+#     cursor =connection.cursor()
+#     try:
+#         truncate_query = f"TRUNCATE TABLE Users"
+#         cursor.execute(truncate_query)
+#
+#         # Reset the auto-increment value to 1
+#         # reset_query = f"ALTER TABLE UserDetails AUTO_INCREMENT = 00001"
+#         # cursor.execute(reset_query)
+#
+#         insert_query = """
+#             INSERT INTO Users (UserID, FirstName, LastName, Age, Ethnicity, Gender)
+#             VALUES (%s, %s, %s, %s, %s, %s)
+#         """
+#         for index, row in table.iterrows():
+#
+#             UserID = 'User_'+"{:03d}".format(index+1)
+#             data = (UserID, row['FirstName'], row['LastName'], int(row['Age']), row['Ethnicity'], row['Gender'])
+#             print(data)
+#             cursor.execute(insert_query,data)
+#         connection.commit()
+#         print('\nData Loaded!')
+#     except mysql.connector.Error as err:
+#         print(f"Error: {err}")
+#
+#     cursor.close()
+#
+#     DB_Disconnect(connection)
+#
+#
+#
+# load_user_data_toSQL(r'M:\Private\Softwares\Python Envs\HyperSpace\ComputerVision\Data\Data_Load_to_sqlDB\UserDetails.xlsx')
 
 
 
--- Facial_Recognition/WebUI/app.py
+++ Facial_Recognition/WebUI/app.py
@@ -1,37 +1,99 @@
 import os
 import cv2
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import sys
 sys.path.append('Facial_Recognition/')
 from Database_Connect.Load_Users import insert_user_info
+def generate_secret_key(length=32):
+    return os.urandom(length).hex()
+
 
 app = Flask(__name__)
+app.secret_key = generate_secret_key()
 
 # Folder to store recorded videos
 UPLOAD_FOLDER = 'Data/Facial_Recog/Raw_DataStore/FacialRecog_TargetVideo'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def main_page():
+    return render_template('index.html')
+
+
+@app.route('/record_mode', methods=['GET', 'POST'])
 def record_mode():
     if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        age = request.form['age']
-        gender = request.form['gender']
-        ethnicity = request.form['ethnicity']
+        first_name = request.form['FirstName']
+        last_name = request.form['LastName']
+        age = request.form['Age']
+        gender = request.form['Ethnicity']
+        ethnicity = request.form['Gender']
+
+        session['FirstName'] = first_name
+        session['LastName'] = last_name
 
         user_info = {
-            'first_name': first_name,
-            'last_name': last_name,
-            'age': age,
-            'gender': gender,
-            'ethnicity': ethnicity
+            'FirstName': first_name,
+            'LastName': last_name,
+            'Age': age,
+            'Ethnicity': gender,
+            'Gender': ethnicity
         }
 
         insert_user_info(user_info)
 
+        return redirect(url_for('record_video_page'))
 
+    return render_template('record_mode.html')
+
+@app.route('/record_video_page', methods=['GET', 'POST'])
+def record_video_page():
+    return render_template('record_video_page.html')
+
+@app.route('/record_video', methods=['GET', 'POST'])
+def record_video():
+    first_name = session.get('FirstName', '')
+    last_name = session.get('LastName', '')
+    if request.method == 'POST':
+        video_path = os.path.join(UPLOAD_FOLDER, f'{first_name}_{last_name}.mp4')
+        capture = cv2.VideoCapture(0)
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        frame_width = int(capture.get(3))
+        frame_height = int(capture.get(4))
+        out = cv2.VideoWriter(video_path, fourcc, 20.0, (frame_width, frame_height))
+
+        # Record for 10 seconds
+        start_time = cv2.getTickCount()
+        while capture.isOpened():
+            ret, frame = capture.read()
+            if not ret:
+                break
+
+            out.write(frame)
+            cv2.imshow('Recording', frame)
+
+            # Stop recording after 10 seconds
+            if (cv2.getTickCount() - start_time) / cv2.getTickFrequency() > 10:
+                break
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        capture.release()
+        out.release()
+        cv2.destroyAllWindows()
+
+        return "Video recorded successfully"
+    return render_template('record_video.html')
+
+@app.route('/test_mode')
+def test_mode():
+    return "Test Mode Page"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
         # # Capture video
         # video_path = os.path.join(UPLOAD_FOLDER, f'{first_name}_{last_name}.mp4')
@@ -54,9 +116,9 @@ def record_mode():
         # out.release()
         # cv2.destroyAllWindows()
         #
-        # return f'Recorded and saved video as {video_path}<br><pre>{user_info}</pre>'
+        # return f'Recorded and `saved video as {video_path}<br><pre>{user_info}</pre>'`
 
-    return render_template('record_mode.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
