60952e7368b38d065756dc1f4547a68dd495d2b3
THEROCKOMANZ\Manz <manasji.haldar@hyperspace-consulting.com>
2023-08-08 17:24:48 +0530

    UserUploading

--- Facial_Recognition/Database_Connect/Load_Users.py
+++ Facial_Recognition/Database_Connect/Load_Users.py
@@ -39,6 +39,45 @@ def insert_user_info(user_info):
 
 
 
+-------------------------------------------------------------------------------------------------------------
+
+def load_user_data_toSQL(XL_file):
+    table = pd.DataFrame(pd.read_excel(XL_file))
+    table = table.where(pd.notna(table), None)
+    connection = DB_Connect()
+    cursor =connection.cursor()
+    try:
+        truncate_query = f"TRUNCATE TABLE Users"
+        cursor.execute(truncate_query)
+
+        # Reset the auto-increment value to 1
+        # reset_query = f"ALTER TABLE UserDetails AUTO_INCREMENT = 00001"
+        # cursor.execute(reset_query)
+
+        insert_query = """
+            INSERT INTO Users (UserID, FirstName, LastName, Age, Ethnicity, Gender)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """
+        for index, row in table.iterrows():
+
+            UserID = 'User_'+"{:03d}".format(index+1)
+            data = (UserID, row['FirstName'], row['LastName'], int(row['Age']), row['Ethnicity'], row['Gender'])
+            print(data)
+            cursor.execute(insert_query,data)
+        connection.commit()
+        print('\nData Loaded!')
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+
+    cursor.close()
+
+    DB_Disconnect(connection)
+
+
+
+load_user_data_toSQL(r'M:\Private\Softwares\Python Envs\HyperSpace\ComputerVision\Data\Data_Load_to_sqlDB\UserDetails.xlsx')
+
+
 
 
 
